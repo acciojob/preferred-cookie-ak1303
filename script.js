7220f0ba6e57fcebc95385bb503b1d0397f071fe
ak1303 <94529241+ak1303@users.noreply.github.com>
2024-03-25 18:32:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+let form = document.getElementsByTagName('form')[0];
+let fontColor = document.getElementById('fontcolor');
+let fontSize = document.getElementById('fontsize');
+if(localStorage.getItem('color')){
+	fontColor.value = localStorage.getItem('color');
+	fontSize.value = localStorage.getItem('size');
+}
+form.addEventListener('submit',(e)=>{
+	e.preventDefault();
+	let color = fontColor.value;
+	let size = fontSize.value;
+	localStorage.setItem('color',color);
+	localStorage.setItem('size',size);
+	fontColor.value = localStorage.getItem('color');
+	fontSize.value = localStorage.getItem('size');
+});
